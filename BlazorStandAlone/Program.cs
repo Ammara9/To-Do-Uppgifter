c8e9f19b8ad923d53a1b976c15e48d29e18f6923
Ammara Shakeel <ammarashakeel81@yahoo.se>
2024-12-20 21:05:47 +0100

    Add project files.

---
+++ BlazorStandAlone/Program.cs
@@ -0,0 +1,25 @@
+using BlazorStandAlone.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace BlazorStandAlone
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress),
+            });
+            //Service for todo
+            builder.Services.AddScoped<GlobalStateToDo>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ BlazorStandAlone/Services/GlobalStateToDo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorStandAlone.Services
+{
+    public class GlobalStateToDo
+    {
+        public List<string?> toDoList { get; set; } = new();
+
+        //public event Action? OnChange;
+
+        public void AddToList(string? item)
+        {
+            toDoList.Add(item);
+        }
+
+        public void Remove(string? item)
+        {
+            toDoList.Remove(item);
+        }
+
+        //public void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
